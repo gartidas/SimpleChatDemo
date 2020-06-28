24fe81c76bb9dc67895ab218f1c7a0edfb5e5c08
Adam <garamone@gmail.com>
2020-06-28 20:27:52 +0200

    First commit

---
+++ WebApi/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Data
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
